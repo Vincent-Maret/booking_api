159deb16d0d20adda2a4d759baaf7939b477a8aa
papaya-industry <maretvincent@protonmail.com>
2022-04-02 17:39:41 +0200

    feat: add restaurants set

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from flask import Flask
 from flask import request
 from flask import Response
 
+restaurants = {
+    'Coin moldu',
+    'Falafel Fix',
+    'Station Ramen',
+    'Amritsari Masala',
+    'Pupsicle',
+    '50 nuances de café',
+    'Au-delà des tacos',
+    'La barre de smoothie',
+    'Loki’s Lounge',
+    'Veggie Wonderland',
+    'Kebab Capital'
+}
+
+
 app = Flask(__name__)
 
 if __name__ == '__main__':
