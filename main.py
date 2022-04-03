0c931f31a17f31f4b54b15845307fb448d7e4f17
papaya-industry <maretvincent@protonmail.com>
2022-04-03 16:06:32 +0200

    feat: add endpoint to book a slot

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from typing import List, Set, Dict, Tuple, Optional, Union, Literal, TypedDict, get_args
-from flask import Flask, abort, jsonify
+import re
 from werkzeug.exceptions import HTTPException
+from flask import Flask, abort, jsonify, request
+
+PHONE_REGEX = re.compile(r"^\d{10}$")
 
 RestaurantId = Literal['coin_moldu',
                        'falafel_fix',
@@ -15,10 +18,18 @@ RestaurantId = Literal['coin_moldu',
                        'kebab_capital']
 
 
+class Booking(TypedDict):
+    '''Booking type'''
+    slot: str
+    name: str
+    phone: str
+
+
 class RestaurantData(TypedDict):
     '''Restaurant data type'''
     name: str
     slots: Set[str]
+    bookings: List[Booking]
 
 
 SLOTS: Set[str] = {'19h', '19h30', '20h', '20h30', '21h', '21h30'}
@@ -29,7 +40,8 @@ restaurants: Dict[RestaurantId, RestaurantData] = {}
 for rid in RESTAURANT_IDS:
     restaurants[rid] = {
         'name': rid.replace('_', ' ').capitalize(),
-        'slots': SLOTS.copy()
+        'slots': SLOTS.copy(),
+        'bookings': []
     }
 
 app = Flask(__name__)
@@ -59,6 +71,35 @@ def get_restaurant_slots(r_id: RestaurantId) -> Dict[str, List[str]]:
         abort(404, 'Restaurant id {} do not exist'.format(r_id))
 
     return {'slots': list(restaurants[r_id]["slots"])}
+
+
+@app.route('/restaurant/<r_id>/book/<slot>', methods=['POST'])
+def book_slot(r_id: RestaurantId, slot: str):
+    '''Book given slot for given restaurant'''
+    if not r_id in RESTAURANT_IDS:
+        abort(404, 'Restaurant id {} do not exist'.format(r_id))
+    elif not slot in restaurants[r_id]['slots']:
+        abort(400, 'Slot {} is not available'.format(slot))
+    elif not isinstance(request.json, dict):
+        abort(400, 'Request format must be json')
+    elif not 'name' in request.json:
+        abort(400, 'A name must be given')
+    elif not 'phone' in request.json:
+        abort(400, 'A phone must be given')
+
+    name = request.json['name']
+    phone = request.json['phone']
+
+    if not isinstance(name, str) or len(name) == 0:
+        abort(400, 'Malformated name')
+    elif not isinstance(phone, str) or not PHONE_REGEX.match(phone):
+        abort(400, 'Malformated phone')
+
+    restaurants[r_id]['bookings'].append(
+        {'slot': slot, 'name': name, 'phone': phone})
+    restaurants[r_id]['slots'].remove(slot)
+
+    return 'Slot {} booked'.format(slot)
 
 
 if __name__ == '__main__':
